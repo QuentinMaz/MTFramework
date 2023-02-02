56e4ca58f667889895feb9049cc32a052878d2c0
Quentin Mazouni <quentinmaz@hotmail.fr>
2023-02-02 14:11:23 +0100

    added new planners + updated Python script to call the fastDownward system.

--- planners/fd_planner.py
+++ planners/fd_planner.py
@@ -4,20 +4,20 @@ import os
 
 TIMEOUT = 120
 WEIGHT = 10
-FD_PATH = 'path_until/downward'
+FD_PATH = 'c:/Users/Quentin/Downloads/downward'
 
 def main(search: str, eval: str, domain: str, problem: str, output: str) -> None:
     sas = f'{output.split(".")[0]}.sas'
     if search == 'astar':
-        command = f'python {FD_PATH}/fast-downward.py --plan-file {output} --sas-file {sas} {domain} {problem} --evaluator "h={eval}()" --search "astar(h())"'
+        command = f'python {FD_PATH}/fast-downward.py --plan-file {output} --sas-file {sas} {domain} {problem} --evaluator "h={eval}()" --search "astar(h)"'
     elif search == 'wastar':
-        command = f'python {FD_PATH}/fast-downward.py --plan-file {output} --sas-file {sas} {domain} {problem} --evaluator "h={eval}()" --search "eager_wastar([h()], w={WEIGHT})"'
+        command = f'python {FD_PATH}/fast-downward.py --plan-file {output} --sas-file {sas} {domain} {problem} --evaluator "h={eval}()" --search "eager_wastar([h], w={WEIGHT})"'
     else:
         print('unknown search parameter.')
         return
 
     try:
-        subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, timeout=TIMEOUT)
+        subprocess.run(command, shell=True, timeout=TIMEOUT)
         os.remove(sas)
     except:
         print(f'astar {eval} error on {domain} {problem}.')
